fc77c169a02fc26b4757943a5e0564b0f75d8956
Alejandro Costa <alejandroesfc@gmail.com>
2022-04-24 07:40:53 -0300

    creates a testServlet and maps it to /test

---
+++ src/main/java/servlets/testServlet.java
@@ -0,0 +1,25 @@
+package servlets;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class testServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public testServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
